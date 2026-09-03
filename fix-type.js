aa49d29747279e524727375bfe440108659c367b
Rashid Reshi <rashidreshi42@gmail.com>
2026-09-03 09:45:16 +0530

    chore: remove scratch scripts from git tracking

--- fix-type.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'apps/delivery-dashboard/src/app/page.tsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/catch \(e\)/g, "catch (e: any)");
-content = content.replace(/catch\(e =>/g, "catch((e: any) =>");
-
-fs.writeFileSync(file, content);
