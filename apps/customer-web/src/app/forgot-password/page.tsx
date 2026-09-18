c54e64676aec528a3be54762615e01865e8fe509
Rashid Reshi <rashidreshi42@gmail.com>
2026-09-18 14:24:09 +0530

    fix(auth): Use correct MSG91 auth key

--- apps/customer-web/src/app/forgot-password/page.tsx
+++ apps/customer-web/src/app/forgot-password/page.tsx
@@ -94,7 +94,7 @@ export default function ForgotPasswordPage() {
       const tokenAuth =
         process.env.NEXT_PUBLIC_MSG91_WIDGET_TOKEN ||
         process.env.NEXT_PUBLIC_MSG91_TOKEN_AUTH ||
-        '556022TLShucwZ86a6d8a7bP1';
+        '556022AwCmehfIN6a6dc465P1';
       const identifier = formatIdentifier(phone);
 
       const configuration = {
